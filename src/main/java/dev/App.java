8e11a6088e2ad9531d4dc625572adbf16d9e815d
Thiblt <t.pouplard@gmx.fr>
2024-05-22 15:05:33 +0200

    TP3

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,9 @@
 package dev;
 
+import java.util.ResourceBundle;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!");
+        //System.out.println( "Hello World!");
+        String titre = ResourceBundle.getBundle("application").getString("titre");
+        String asciiArt = FigletFont.convertOneLine(titre);
+        System.out.println(asciiArt);
+        
+     // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString(
+        "environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
